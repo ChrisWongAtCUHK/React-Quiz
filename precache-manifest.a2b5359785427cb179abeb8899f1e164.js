9d1ae1b8622450821391e47c891ca33122eea7d9
chriswongatcuhk <wc38.wong@gmail.com>
2019-06-07 08:12:51 +0800

    Updates

---
+++ precache-manifest.a2b5359785427cb179abeb8899f1e164.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56123f8ecb9b75b77acf879c6bff6f50",
+    "url": "/React-Quiz/index.html"
+  },
+  {
+    "revision": "04b0338ca213fe8eb1d3",
+    "url": "/React-Quiz/static/js/2.88f0dd14.chunk.js"
+  },
+  {
+    "revision": "03465097dfe99683aa06",
+    "url": "/React-Quiz/static/js/main.e673eaf0.chunk.js"
+  },
+  {
+    "revision": "0ba59119827f22c0af1b",
+    "url": "/React-Quiz/static/js/runtime~main.231c92fb.js"
+  }
+]);
